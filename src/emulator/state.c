85c09892444406b160267145398950f4817115e2
Rohit Bhatta <rbhatta@cs.utexas.edu>
2016-02-29 00:07:27 -0600

    Didn't pass

--- src/emulator/state.c
+++ src/emulator/state.c
@@ -27,9 +27,9 @@ void run(State* s) {
     int exit = 0;
     while (!exit) {
         uint32_t instr = read32(memory, s -> pc);
-        uint16_t opcode = (uint16_t) (instr >> 26 & 0x3F);
-        /*printf("%lu\n", s -> pc);
-        printf("%s%d\n", "opcode", opcode);
+        uint16_t opcode = (uint16_t) ((instr >> 26) & 0x3F);
+        //printf("%lx\n", s -> pc);
+        /*printf("%s%d\n", "opcode", opcode);
         for (int i = 0; i < 32; i++) {
             printf("%d: ", i);
             printf("%lu\n", s -> gprs[i]);
@@ -37,7 +37,8 @@ void run(State* s) {
         switch (opcode) {
             //cmpdi
             case 11 : {
-                uint16_t srcA = (uint16_t) (instr >> 16 & 0x1F);
+                //printf("%s\n", "cmpdi");
+                uint16_t srcA = (uint16_t) ((instr >> 16) & 0x1F);
                 int imm = (int) (instr << 16);
                 imm = imm >> 16;
                 if ((s -> gprs[srcA]) < (imm)) {
@@ -54,16 +55,18 @@ void run(State* s) {
             }
             //addi, li
             case 14 : {
-                uint16_t dest = (uint16_t) (instr >> 21 & 0x1F);
-                uint16_t src = (uint16_t) (instr >> 16 & 0x1F);
+                uint16_t dest = (uint16_t) ((instr >> 21) & 0x1F);
+                uint16_t src = (uint16_t) ((instr >> 16) & 0x1F);
                 int imm = (int) (instr << 16);
                 imm = imm >> 16;
                 //addi
                 if (src != 0) {
+                //printf("%s\n", "addi");
                     s -> gprs[dest] = s -> gprs[src] + imm;
                 }
                 //li
                 else {
+                //printf("%s\n", "li");
                     s -> gprs[dest] = imm;
                 }
                 s -> pc += 4;
@@ -71,16 +74,24 @@ void run(State* s) {
             }
             //lis
             case 15 : {
-                uint16_t dest = (uint16_t) (instr >> 21 & 0x1F);
+                //printf("%s\n", "lis");
+                uint16_t dest = (uint16_t) ((instr >> 21) & 0x1F);
+                uint16_t src = (uint16_t) ((instr >> 16) & 0x1F);
                 int imm = (int) (instr << 16 & 0xFFFF0000);
-                s -> gprs[dest] = imm;
+                if (src == 0) {
+                    s -> gprs[dest] = imm;
+                }
+                else {
+                    s -> gprs[dest] = s -> gprs[src] + imm;
+                }
                 s -> pc += 4;
                 break;
             }
             //branch conditionals: beq, bne, ble, bge
             case 16 : {
-                uint16_t bo = (uint16_t) (instr >> 23 & 0x7);
-                uint16_t bi = (uint16_t) (instr >> 16 & 0x1F);
+                //printf("%s\n", "bc");
+                uint16_t bo = (uint16_t) ((instr >> 23) & 0x7);
+                uint16_t bi = (uint16_t) ((instr >> 16) & 0x1F);
                 int target = (int) (instr << 16);
                 target = target >> 18;
                 target = target << 2;
@@ -113,10 +124,12 @@ void run(State* s) {
             case 17 : {
                 //exit
                 if (s -> gprs[0] == 1) {
+                //printf("%s\n", "exit");
                     exit = 1;
                 }
                 //print
                 else if (s -> gprs[0] == 4) {
+                //printf("%s\n", "print");
                     uint64_t r4 = s -> gprs[4];
                     char printNum = read8(memory, r4);
                     printf("%c", printNum);
@@ -133,11 +146,13 @@ void run(State* s) {
                 switch (ext) {
                     //b
                     case 0 : {
+                //printf("%s\n", "b");
                         s -> pc += eAddr;
                         break;
                     }
                     //bl
                     case 1 : {
+                //printf("%s\n", "bl");
                         s -> lr = s -> pc + 4;
                         s -> pc += eAddr;
                         break;
@@ -147,13 +162,15 @@ void run(State* s) {
             }
             //blr, branch to linked register
             case 19 : {
+                //printf("%s\n", "blr");
                 s -> pc = s -> lr;
                 break;
             }
             //ori
             case 24 : {
-                uint16_t dest = (uint16_t) (instr >> 16 & 0x1F);
-                uint16_t src = (uint16_t) (instr >> 21 & 0x1F);
+                //printf("%s\n", "ori");
+                uint16_t dest = (uint16_t) ((instr >> 16) & 0x1F);
+                uint16_t src = (uint16_t) ((instr >> 21) & 0x1F);
                 uint64_t imm = (uint64_t) (instr & 0xFFFF);
                 s -> gprs[dest] = s -> gprs[src] | imm;
                 s -> pc += 4;
@@ -161,8 +178,9 @@ void run(State* s) {
             }
             //oris
             case 25 : {
-                uint16_t dest = (uint16_t) (instr >> 16 & 0x1F);
-                uint16_t src = (uint16_t) (instr >> 21 & 0x1F);
+                //printf("%s\n", "oris");
+                uint16_t dest = (uint16_t) ((instr >> 16) & 0x1F);
+                uint16_t src = (uint16_t) ((instr >> 21) & 0x1F);
                 uint64_t imm = (uint64_t) (instr & 0xFFFF);
                 imm = imm << 16 & 0xFFFF0000;
                 s -> gprs[dest] = s -> gprs[src] | imm;
@@ -171,7 +189,8 @@ void run(State* s) {
             }
             //rldicr
             case 30 : {
-                uint16_t dest = (uint16_t) (instr >> 16 & 0x1F);
+                //printf("%s\n", "rldicr");
+                uint16_t dest = (uint16_t) ((instr >> 16) & 0x1F);
                 uint64_t temp = s -> gprs[dest];
                 temp = temp << 32 & 0xFFFFFFFF00000000;
                 s -> gprs[dest] = temp;
@@ -184,66 +203,74 @@ void run(State* s) {
                 switch (ext) {
                     //add
                     case 266 : {
-                        uint16_t dest = (uint16_t) (instr >> 21 & 0x1F);
-                        uint16_t srcA = (uint16_t) (instr >> 16 & 0x1F);
-                        uint16_t srcB = (uint16_t) (instr >> 11 & 0x1F);
+                //printf("%s\n", "add");
+                        uint16_t dest = (uint16_t) ((instr >> 21) & 0x1F);
+                        uint16_t srcA = (uint16_t) ((instr >> 16) & 0x1F);
+                        uint16_t srcB = (uint16_t) ((instr >> 11) & 0x1F);
                         s -> gprs[dest] = (s -> gprs[srcA]) + (s -> gprs[srcB]);
                         s -> pc += 4;
                         break;
                     }
                     //subf
                     case 40 : {
-                        uint16_t dest = (uint16_t) (instr >> 21 & 0x1F);
-                        uint16_t srcA = (uint16_t) (instr >> 16 & 0x1F);
-                        uint16_t srcB = (uint16_t) (instr >> 11 & 0x1F);
+                //printf("%s\n", "subf");
+                        uint16_t dest = (uint16_t) ((instr >> 21) & 0x1F);
+                        uint16_t srcA = (uint16_t) ((instr >> 16) & 0x1F);
+                        uint16_t srcB = (uint16_t) ((instr >> 11) & 0x1F);
                         s -> gprs[dest] = (s -> gprs[srcB]) - (s -> gprs[srcA]);
                         s -> pc += 4;
                         break;
                     }
                     //mulld, might be 235
                     case 233 : {
-                        uint16_t dest = (uint16_t) (instr >> 21 & 0x1F);
-                        uint16_t srcA = (uint16_t) (instr >> 16 & 0x1F);
-                        uint16_t srcB = (uint16_t) (instr >> 11 & 0x1F);
+                //printf("%s\n", "mulld");
+                        uint16_t dest = (uint16_t) ((instr >> 21) & 0x1F);
+                        uint16_t srcA = (uint16_t) ((instr >> 16) & 0x1F);
+                        uint16_t srcB = (uint16_t) ((instr >> 11) & 0x1F);
                         s -> gprs[dest] = (s -> gprs[srcA]) * (s -> gprs[srcB]);
                         s -> pc += 4;
                         break;
                     }
                     //divdu
                     case 457 : {
-                        uint16_t dest = (uint16_t) (instr >> 21 & 0x1F);
-                        uint16_t srcA = (uint16_t) (instr >> 16 & 0x1F);
-                        uint16_t srcB = (uint16_t) (instr >> 11 & 0x1F);
+                //printf("%s\n", "divdu");
+                        uint16_t dest = (uint16_t) ((instr >> 21) & 0x1F);
+                        uint16_t srcA = (uint16_t) ((instr >> 16) & 0x1F);
+                        uint16_t srcB = (uint16_t) ((instr >> 11) & 0x1F);
                         s -> gprs[dest] = (s -> gprs[srcA])/(s -> gprs[srcB]);
                         s -> pc += 4;
                         break;
                     }
                     //mr
                     case 444 : {
-                        uint16_t dest = (uint16_t) (instr >> 16 & 0x1F);
-                        uint16_t src = (uint16_t) (instr >> 21 & 0x1F);
+                //printf("%s\n", "mr");
+                        uint16_t dest = (uint16_t) ((instr >> 16) & 0x1F);
+                        uint16_t src = (uint16_t) ((instr >> 21) & 0x1F);
                         s -> gprs[dest] = s -> gprs[src];
                         s -> pc += 4;
                         break;
                     }
                     //mflr
                     case 339 : {
-                        uint16_t dest = (uint16_t) (instr >> 21 & 0x1F);
+                //printf("%s\n", "mflr");
+                        uint16_t dest = (uint16_t) ((instr >> 21) & 0x1F);
                         s -> gprs[dest] = s -> lr;
                         s -> pc += 4;
                         break;
                     }
                     //mtlr
                     case 467 : {
-                        uint16_t src = (uint16_t) (instr >> 21 & 0x1F);
+                //printf("%s\n", "mtlr");
+                        uint16_t src = (uint16_t) ((instr >> 21) & 0x1F);
                         s -> lr = s -> gprs[src];
                         s -> pc += 4;
                         break;
                     }
                     //cmpd
                     case 0 : {
-                        uint16_t srcA = (uint16_t) (instr >> 16 & 0x1F);
-                        uint16_t srcB = (uint16_t) (instr >> 11 & 0x1F);
+                //printf("%s\n", "cmpd");
+                        uint16_t srcA = (uint16_t) ((instr >> 16) & 0x1F);
+                        uint16_t srcB = (uint16_t) ((instr >> 11) & 0x1F);
                         if ((s -> gprs[srcA]) < (s -> gprs[srcB])) {
                             s -> cr = 0x80000000;
                         }
@@ -258,9 +285,10 @@ void run(State* s) {
                     }
                     //stbx
                     case 215 : {
-                        uint16_t dest = (uint16_t) (instr >> 21 & 0x1F);
-                        uint16_t srcA = (uint16_t) (instr >> 16 & 0x1F);
-                        uint16_t srcB = (uint16_t) (instr >> 10 & 0x3F);
+                //printf("%s\n", "stbx");
+                        uint16_t dest = (uint16_t) ((instr >> 21) & 0x1F);
+                        uint16_t srcA = (uint16_t) ((instr >> 16) & 0x1F);
+                        uint16_t srcB = (uint16_t) ((instr >> 10) & 0x3F);
                         uint64_t eAddr;
                         if (srcA == 0) {
                             eAddr = s -> gprs[srcB];
@@ -282,6 +310,7 @@ void run(State* s) {
             }
             //stb
             case 38 : {
+                //printf("%s\n", "stb");
                 uint16_t dest = (uint16_t) (instr >> 21 & 0x1F);
                 uint16_t src = (uint16_t) (instr >> 16 & 0x1F);
                 int imm = (int) (instr << 16);
@@ -301,8 +330,9 @@ void run(State* s) {
             //ld
             //Possibly add if src = 0, add imm to 0
             case 58 : {
-                uint16_t dest = (uint16_t) (instr >> 21 & 0x1F);
-                uint16_t src = (uint16_t) (instr >> 16 & 0x1F);
+                //printf("%s\n", "ld");
+                uint16_t dest = (uint16_t) ((instr >> 21) & 0x1F);
+                uint16_t src = (uint16_t) ((instr >> 16) & 0x1F);
                 int imm = (int) (instr << 16);
                 imm = imm >> 18;
                 imm = imm << 2;
@@ -325,8 +355,9 @@ void run(State* s) {
                 switch (ext) {
                     //std
                     case 0 : {
-                        uint16_t dest = (uint16_t) (instr >> 21 & 0x1F);
-                        uint16_t src = (uint16_t) (instr >> 16 & 0x1F);
+                //printf("%s\n", "std");
+                        uint16_t dest = (uint16_t) ((instr >> 21) & 0x1F);
+                        uint16_t src = (uint16_t) ((instr >> 16) & 0x1F);
                         int imm = (int) (instr << 16);//Might need to change to 0xFFFC
                         imm = imm >> 18;
                         imm = imm << 2;
@@ -343,8 +374,9 @@ void run(State* s) {
                     }
                     //stdu
                     case 1 : {
-                        uint16_t dest = (uint16_t) (instr >> 21 & 0x1F);
-                        uint16_t src = (uint16_t) (instr >> 16 & 0x1F);
+                //printf("%s\n", "stdu");
+                        uint16_t dest = (uint16_t) ((instr >> 21) & 0x1F);
+                        uint16_t src = (uint16_t) ((instr >> 16) & 0x1F);
                         int imm = (int) (instr << 16);
                         imm = imm >> 18;
                         imm = imm << 2;
